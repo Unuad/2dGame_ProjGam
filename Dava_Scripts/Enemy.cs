9e5b6e8ef862c2196b5110ce9460d24ed4df636d
Unuad <evtishinby@gmail.com>
2021-11-29 22:28:04 +0300

    22.27
    code

--- Dava_Scripts/Enemy.cs
+++ Dava_Scripts/Enemy.cs
@@ -4,15 +4,17 @@ using System;
 public class Enemy : KinematicBody2D {
 	int fall_speed = 1;
     static Vector2 vel = new Vector2();
-    
-
     int cor_x = GetRandom(1024);
     Sprite spr;
+
+    Node main;
+
+    CollisionShape2D colis;
 
     static int GetRandom(int x) {
         //Создание объекта для генерации чисел (с указанием начального значения)
         Random rnd = new Random();
- 
+
         //Получить случайное число 
         int value = rnd.Next(x);
  
@@ -23,16 +25,25 @@ public class Enemy : KinematicBody2D {
     public override void _Ready() {
         this.Position += new Vector2(cor_x, 0);
         spr = GetNode("/root/Main/Enemy/Enemy") as Sprite;
+        colis = GetNode("/root/Main/Enemy/CollisionShape2D") as CollisionShape2D;
+        main = GetNode("/root/Main") as Node;
     }
 
-	
-	public override void _PhysicsProcess(float delta) {	
-           //GD.Print(cor_x);
-        this.Position += new Vector2(0, fall_speed);
+    public void Check(float delta) {
         if (this.Position.y >= 560){
-            //GD.Print("i must be killed");
             QueueFree();
-        }   
+        }
+        var collisionInfo = MoveAndCollide(vel * delta);
+        if (collisionInfo != null) {
+            //main.points += 1;
+            QueueFree();
+        }
+    }
+
+	public override void _PhysicsProcess(float delta) {	
+        this.Position += new Vector2(0, fall_speed);
+        Check(delta);
+        //MoveAndCollide(vel.Normalized() * fall_speed * delta);
     }
 
 }
