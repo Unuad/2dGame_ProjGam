9ec78475a2dfe95c3df074c3bb947bc112e98df9
Vingo241 <kameyko.as@gmail.com>
2021-11-29 22:13:06 +0300

    1
    1

--- Dava_Scripts/Enemy.cs
+++ Dava_Scripts/Enemy.cs
@@ -3,36 +3,36 @@ using System;
 
 public class Enemy : KinematicBody2D {
 	int fall_speed = 1;
-    static Vector2 vel = new Vector2();
-    
+	static Vector2 vel = new Vector2();
+	
 
-    int cor_x = GetRandom(1024);
-    Sprite spr;
+	int cor_x = GetRandom(1024);
+	Sprite spr;
 
-    static int GetRandom(int x) {
-        //Создание объекта для генерации чисел (с указанием начального значения)
-        Random rnd = new Random();
+	static int GetRandom(int x) {
+		//Создание объекта для генерации чисел (с указанием начального значения)
+		Random rnd = new Random();
  
-        //Получить случайное число 
-        int value = rnd.Next(x);
+		//Получить случайное число 
+		int value = rnd.Next(x);
  
-        //Вернуть полученное значение
-        return value;
-    }
+		//Вернуть полученное значение
+		return value;
+	}
 
-    public override void _Ready() {
-        this.Position += new Vector2(cor_x, 0);
-        spr = GetNode("/root/Main/Enemy/Enemy") as Sprite;
-    }
+	public override void _Ready() {
+		this.Position += new Vector2(cor_x, 0);
+		spr = GetNode("/root/Main/Enemy/Enemy") as Sprite;
+	}
 
 	
 	public override void _PhysicsProcess(float delta) {	
-           //GD.Print(cor_x);
-        this.Position += new Vector2(0, fall_speed);
-        if (this.Position.y >= 560){
-            //GD.Print("i must be killed");
-            QueueFree();
-        }   
-    }
+		   //GD.Print(cor_x);
+		this.Position += new Vector2(0, fall_speed);
+		if (this.Position.y >= 560){
+			//GD.Print("i must be killed");
+			QueueFree();
+		}   
+	}
 
 }
