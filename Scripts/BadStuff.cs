4b0f50edc52b6075764cb60344d377f603258c81
Vingo241 <kameyko.as@gmail.com>
2021-12-19 19:24:31 +0300

    buttons
    asadedhr

--- Scripts/BadStuff.cs
+++ Scripts/BadStuff.cs
@@ -3,58 +3,58 @@ using System;
 
 namespace Game
 {
-    public class BadStuff : KinematicBody2D
-    {
-        int fall_speed = 1;
-        public static Vector2 _screenSize;
-        static Vector2 vel = new Vector2();
-        int cor_x = GetRandom(Convert.ToInt32(_screenSize.x));
-        private int points = 5;
+	public class BadStuff : KinematicBody2D
+	{
+		int fall_speed = 1;
+		public static Vector2 _screenSize;
+		static Vector2 vel = new Vector2();
+		int cor_x = GetRandom(Convert.ToInt32(_screenSize.x));
+		private int points = 5;
 
 
-        static int GetRandom(int x)
-        {
-            Random rnd = new Random();
-            int value = rnd.Next(x);
-            return value;
-        }
+		static int GetRandom(int x)
+		{
+			Random rnd = new Random();
+			int value = rnd.Next(x);
+			return value;
+		}
 
-        public override void _Ready()
-        {
-            this.Position += new Vector2(cor_x, 0);
-            _screenSize = GetViewport().Size;
-        }
+		public override void _Ready()
+		{
+			this.Position += new Vector2(cor_x, 0);
+			_screenSize = GetViewport().Size;
+		}
 
-        public void Check(float delta)
-        {   
-            var collisionInfo = MoveAndCollide(vel * delta);
-            if (collisionInfo != null)
-            {
-                if (Convert.ToString(collisionInfo.GetCollider().GetType()) == "Game.Player")
-                {
-                    QueueFree();
-                    Player.points += points;
-                    GD.Print(Player.points);
-                }
-            }
+		public void Check(float delta)
+		{   
+			var collisionInfo = MoveAndCollide(vel * delta);
+			if (collisionInfo != null)
+			{
+				if (Convert.ToString(collisionInfo.GetCollider().GetType()) == "Game.Player")
+				{
+					QueueFree();
+					Player.points += points;
+					GD.Print(Player.points);
+				}
+			}
 
-            if (this.Position.y > _screenSize.y + 50)
-            {
-                //GD.Print("im out of bounds");
-                QueueFree();
-            }
-        }
+			if (this.Position.y > _screenSize.y + 50)
+			{
+				//GD.Print("im out of bounds");
+				QueueFree();
+			}
+		}
 
-        public override void _PhysicsProcess(float delta)
-        {
-            _screenSize = GetViewport().Size;
-            this.Position += new Vector2(0, fall_speed);
-            if (this.Position.y >= 700)
-            {
-                QueueFree();
-            }
+		public override void _PhysicsProcess(float delta)
+		{
+			_screenSize = GetViewport().Size;
+			this.Position += new Vector2(0, fall_speed);
+			if (this.Position.y >= 700)
+			{
+				QueueFree();
+			}
 
-            Check(delta);
-        }
-    }
+			Check(delta);
+		}
+	}
 }
--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -3,44 +3,44 @@ using System;
 
 namespace Game
 {
-    public class Main : Node2D
-    { 
-        public static Vector2 _screenSize; 
-        private int cor_x;
-        private static PackedScene Apple = ResourceLoader.Load<PackedScene>("res://Scenes/Apple.tscn");
-        private static PackedScene Dumbbell = ResourceLoader.Load<PackedScene>("res://Scenes/Dumbbell.tscn");
-        private static PackedScene Book = ResourceLoader.Load<PackedScene>("res://Scenes/Book.tscn");
-        private static PackedScene Banana = ResourceLoader.Load<PackedScene>("res://Scenes/Banana.tscn");
-        private static PackedScene Cigarette = ResourceLoader.Load<PackedScene>("res://Scenes/Cigarette.tscn");
-        private static PackedScene Beer = ResourceLoader.Load<PackedScene>("res://Scenes/Beer.tscn");
-        private static PackedScene Tiktok = ResourceLoader.Load<PackedScene>("res://Scenes/Tiktok.tscn");
-        private static PackedScene Gamburger = ResourceLoader.Load<PackedScene>("res://Scenes/Gamburger.tscn");
+	public class Main : Node2D
+	{ 
+		public static Vector2 _screenSize; 
+		private int cor_x;
+		private static PackedScene Apple = ResourceLoader.Load<PackedScene>("res://Scenes/Apple.tscn");
+		private static PackedScene Dumbbell = ResourceLoader.Load<PackedScene>("res://Scenes/Dumbbell.tscn");
+		private static PackedScene Book = ResourceLoader.Load<PackedScene>("res://Scenes/Book.tscn");
+		private static PackedScene Banana = ResourceLoader.Load<PackedScene>("res://Scenes/Banana.tscn");
+		private static PackedScene Cigarette = ResourceLoader.Load<PackedScene>("res://Scenes/Cigarette.tscn");
+		private static PackedScene Beer = ResourceLoader.Load<PackedScene>("res://Scenes/Beer.tscn");
+		private static PackedScene Tiktok = ResourceLoader.Load<PackedScene>("res://Scenes/Tiktok.tscn");
+		private static PackedScene Gamburger = ResourceLoader.Load<PackedScene>("res://Scenes/Gamburger.tscn");
 
-        private PackedScene[] all_stuff = new PackedScene[8] {Apple, Dumbbell, Book, Banana, Cigarette, Beer, Tiktok, Gamburger};
-        
-       static int GetRandom(int x)
-        {
-            Random rnd = new Random();
-            int value = rnd.Next(x);
-            return value;
-        }
-        public override void _Ready()
-        {
-            
-            _screenSize = GetViewport().Size;
-            // Timer
-            Timer timer = this.GetNode<Timer>("/root/Main/Clock");
-            timer.WaitTime = 1;
-            timer.Connect("timeout", this, "Spawn");
-            timer.Start();
-        }
+		private PackedScene[] all_stuff = new PackedScene[8] {Apple, Dumbbell, Book, Banana, Cigarette, Beer, Tiktok, Gamburger};
+		
+	   static int GetRandom(int x)
+		{
+			Random rnd = new Random();
+			int value = rnd.Next(x);
+			return value;
+		}
+		public override void _Ready()
+		{
+			
+			_screenSize = GetViewport().Size;
+			// Timer
+			Timer timer = this.GetNode<Timer>("/root/Main/Clock");
+			timer.WaitTime = 1;
+			timer.Connect("timeout", this, "Spawn");
+			timer.Start();
+		}
 
-        public void Spawn()
-        {
-            Godot.KinematicBody2D thing = all_stuff[GetRandom(7)].Instance() as KinematicBody2D;
-            AddChild(thing);
-            //GD.Print("Im alive");
-        }
+		public void Spawn()
+		{
+			Godot.KinematicBody2D thing = all_stuff[GetRandom(7)].Instance() as KinematicBody2D;
+			AddChild(thing);
+			//GD.Print("Im alive");
+		}
 
-    }
+	}
 }
