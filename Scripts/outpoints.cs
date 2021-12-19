931d7d4cfe28391770d651c48eb97351d2af2207
Unuad <evtishinby@gmail.com>
2021-12-19 21:25:51 +0300

    21.25
    yeaa

--- Scripts/outpoints.cs
+++ Scripts/outpoints.cs
@@ -4,20 +4,16 @@ namespace Game
 {
 	public class outpoints : HBoxContainer
 	{
-		// Declare member variables here. Examples:
-		// private int a = 2;
-		// private string b = "text";
-
-		// Called when the node enters the scene tree for the first time.
+		private int number_label;
 		public override void _Ready()
 		{
-			var number_label = Player.points;
+			number_label = Player.points;
 		}
 
-	//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-	//  public override void _Process(float delta)
-	//  {
-	//      
-	//  }
+	
+		public override void _Process(float delta)
+		{
+			number_label = Player.points;
+		}
 	}
 }
